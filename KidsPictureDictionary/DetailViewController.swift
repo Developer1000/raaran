b455ef4a8979f0fc2267603e474c983ef7772b30
rahulrahul208 <imemsp@MACLT016797.local>
2015-04-22 21:26:24 +0530

    Final App with Animal,Transport and Vegetables images
    Final App with Animal,Transport and Vegetables images

--- KidsPictureDictionary/DetailViewController.swift
+++ KidsPictureDictionary/DetailViewController.swift
@@ -14,32 +14,32 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myLabel: UILabel!
-    
+
     var currImage: UIImage?
     var textHeading: String?
     var arrImg:Array<UIImage> = []
-    var arrImgNames:NSArray = []
-    var cateoryName:NSString = ""
-    var subCategoryName:NSString = ""
-    
+	  var arrImgNames:NSArray = []
+  	var cateoryName:NSString = ""
+		var subCategoryName:NSString = ""
+
     var currentIndex = 0
-    var avAudioPlayer:AVAudioPlayer!
+	  var avAudioPlayer:AVAudioPlayer!
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        
-        var name: NSString = arrImgNames.objectAtIndex(0) as NSString
-        
-        var path = NSBundle.mainBundle().pathForResource(name, ofType:constants.keyImgTypeJPG, inDirectory:"\(cateoryName)/\(subCategoryName)/\(constants.imageFolder)/")
-        
-        myImageView.image = UIImage(contentsOfFile:path!)
-        
-        
-        playAudioSound(cateoryName, subCategoryName:subCategoryName, fileName:name)
-        var elementName = DataHelper.getCategoryElementName(cateoryName, subCategoryName: subCategoryName, ElementId: name)
-        myLabel.text = elementName
-        
+			
+			var name: NSString = arrImgNames.objectAtIndex(0) as NSString
+			
+			var path = NSBundle.mainBundle().pathForResource(name, ofType:constants.keyImgTypeJPG, inDirectory:"\(cateoryName)/\(subCategoryName)/\(constants.imageFolder)/")
+
+			myImageView.image = UIImage(contentsOfFile:path!)
+			
+			
+			playAudioSound(cateoryName, subCategoryName:subCategoryName, fileName:name)
+			var elementName = DataHelper.getCategoryElementName(cateoryName, subCategoryName: subCategoryName, ElementId: name)
+			 myLabel.text = elementName
+
         let swipL = UISwipeGestureRecognizer(target: self, action: "swipe:")
         swipL.direction = .Left
         self.view.addGestureRecognizer(swipL)
@@ -48,13 +48,13 @@ class DetailViewController: UIViewController {
         let swipR = UISwipeGestureRecognizer(target: self, action: "swipe:")
         swipR.direction = .Right
         self.view.addGestureRecognizer(swipR)
-        
-        
+
+
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
     
     
     func swipe(gest: UISwipeGestureRecognizer)
@@ -76,7 +76,7 @@ class DetailViewController: UIViewController {
             animateImg(gest.direction == .Left ? true : false)
         }
     }
-    
+
     
     func animateImg(fromLeft : Bool)
     {
@@ -87,34 +87,44 @@ class DetailViewController: UIViewController {
         slideInFromLeftTransition.duration = 0.5
         slideInFromLeftTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         slideInFromLeftTransition.fillMode = kCAFillModeRemoved
+			
+			
+			var name: NSString = arrImgNames.objectAtIndex(currentIndex) as NSString
+			var imageName = "\(cateoryName)/\(subCategoryName)/image/\(name)\(constants.keyImgTypeJPG)"
+			
+			var path = NSBundle.mainBundle().resourcePath?.stringByAppendingPathComponent(imageName)
+			
+			myImageView.image = UIImage(contentsOfFile:path!)
+
+			var elementName = DataHelper.getCategoryElementName(cateoryName, subCategoryName: subCategoryName, ElementId: name)
+			myLabel.text = elementName
+			
+			self.myImageView.layer.addAnimation(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
         
-        
-        var name: NSString = arrImgNames.objectAtIndex(currentIndex) as NSString
-        var imageName = "\(cateoryName)/\(subCategoryName)/image/\(name)\(constants.keyImgTypeJPG)"
-        
-        var path = NSBundle.mainBundle().resourcePath?.stringByAppendingPathComponent(imageName)
-        
-        myImageView.image = UIImage(contentsOfFile:path!)
-        
-        var elementName = DataHelper.getCategoryElementName(cateoryName, subCategoryName: subCategoryName, ElementId: name)
-        myLabel.text = elementName
-        
-        self.myImageView.layer.addAnimation(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
-        
-        playAudioSound(cateoryName, subCategoryName:subCategoryName, fileName:name)
+       playAudioSound(cateoryName, subCategoryName:subCategoryName, fileName:name)
     }
-    
-    func playAudioSound(forCategory:String,subCategoryName:NSString ,fileName:String)
-    {
-        var languageSelected = DataHelper.languageSelected()
-        var soundFileName = "\(forCategory)/\(subCategoryName)/sounds/\(languageSelected)/)"
-        var soundFilePath = NSBundle.mainBundle().pathForResource(fileName, ofType:"\(constants.keySoundFileType)", inDirectory:"\(forCategory)/\(subCategoryName)/\(constants.soundFolder)/\(languageSelected)/")
-        
-        var error:NSError?
-        var url:NSURL = NSURL(fileURLWithPath:soundFilePath!)!
-        avAudioPlayer = AVAudioPlayer(contentsOfURL:url ,error: &error)
-        avAudioPlayer?.prepareToPlay();
-        avAudioPlayer?.play();
+
+	func playAudioSound(forCategory:String,subCategoryName:NSString ,fileName:String)
+	{
+		var languageSelected = DataHelper.languageSelected()
+        if (languageSelected.utf16Count > 0)
+        {
+            var soundFileName = "\(forCategory)/\(subCategoryName)/sounds/\(languageSelected)/)"
+            
+            var soundFilePath = NSBundle.mainBundle().pathForResource(fileName, ofType:"\(constants.keySoundFileType)", inDirectory:"\(forCategory)/\(subCategoryName)/\(constants.soundFolder)/\(languageSelected)/")
+            if soundFilePath != nil
+            {
+                var error:NSError?
+                var url:NSURL = NSURL(fileURLWithPath:soundFilePath!)!
+                avAudioPlayer = AVAudioPlayer(contentsOfURL:url ,error: &error)
+                avAudioPlayer?.prepareToPlay();
+                avAudioPlayer?.play();
+            }
+            
+
+ 
+        }
     }
+
 }
 
