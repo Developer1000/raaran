794d16d9e711cf263d58f74d8ec3e1244456f7b8
SPanigrahi <sp444@ILCH-MD0051.local>
2016-03-22 10:45:08 -0500

    added rest kit

--- KidsPictureDictionary/ItemViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ItemViewController.swift
-//  KidsPictureDictionary
-//
-//  Created by IMEMSP on 23/03/15.
-//  Copyright (c) 2015 Cognizant Technology Solutions. All rights reserved.
-//
-
-import Foundation
